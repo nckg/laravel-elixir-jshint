fb00c34bdc91f1220a6dbfc0855ba3ead0948e8b
Nick Goris <nick@robarov.be>
2015-10-22 10:36:39 +0200

    init

--- index.js
+++ index.js
@@ -7,39 +7,39 @@ var elixir = require('laravel-elixir');
 var path   = require('path');
 
 elixir.extend('jshint', function(src, options) {
-  src = src || [
-    'public/js/**/*.js',
-    '!public/js/vendor/**/*.js'
-  ];
+    src = src || [
+        'public/js/**/*.js',
+        '!public/js/vendor/**/*.js'
+    ];
 
-  options = options || {};
+    options = options || {};
 
-  var onError = function(err) {
-    notify.onError({
-      title: 'Laravel Elixir',
-      subtitle: 'JSHint failed.',
-      message: '<%= error.message %>',
-      icon: path.join(__dirname, '../laravel-elixir/icons/fail.png')
-    })(err);
+    var onError = function(err) {
+        notify.onError({
+            title: 'Laravel Elixir',
+            subtitle: 'JSHint failed.',
+            message: '<%= error.message %>',
+            icon: path.join(__dirname, '../laravel-elixir/icons/fail.png')
+        })(err);
 
-    this.emit('end');
-  };
+        this.emit('end');
+    };
 
-  gulp.task('jshint', function() {
-    return gulp.src(src)
-      .pipe(jshint(options))
-      .pipe(jshint.reporter('jshint-stylish'))
-      .pipe(jshint.reporter('fail'))
-      .on('error', onError)
-      .pipe(notify({
-        title: 'Laravel Elixir',
-        message: 'JSHint passed',
-        icon: path.join(__dirname, '../laravel-elixir/icons/pass.png'),
-        onLast: true
-      }));
-  });
+    gulp.task('jshint', function() {
+        return gulp.src(src)
+            .pipe(jshint(options))
+            .pipe(jshint.reporter('jshint-stylish'))
+            .pipe(jshint.reporter('fail'))
+            .on('error', onError)
+            .pipe(notify({
+                title: 'Laravel Elixir',
+                message: 'JSHint passed',
+                icon: path.join(__dirname, '../laravel-elixir/icons/pass.png'),
+                onLast: true
+            }));
+        });
 
-  this.registerWatcher('jshint', src);
+    this.registerWatcher('jshint', src);
 
-  return this.queueTask('jshint');
+    return this.queueTask('jshint');
 });
